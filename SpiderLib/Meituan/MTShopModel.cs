aef5cb88ef1a758fc70ac9d0812c89e121a3cee0
tiwlin <tiw480876@sina.com>
2014-07-09 09:45:42 +0800

    add the log dll and MTShopModel

---
+++ SpiderLib/Meituan/MTShopModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Model;
+
+namespace SpiderLib.Meituan
+{
+
+    public class MTShopModel : ShopModel
+    {
+
+    }
+
+    public class Poilevel
+    {
+
+    }
+}
